5ae00215c4ba0338ca33e16e7e4986bcf356d541
Rajat Shrivastava <rajats643@gmail.com>
2024-11-04 12:46:23 -0800

    initial commit

---
+++ scripts/utils.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import numpy as np
+import cv2 as cv
+import logging.config
+
+if __name__ == "__main__":
+    print("utility functions for image segmentation")
+    print(f"using: \n open-cv:{cv.__version__}\n numpy {np.__version__}")
