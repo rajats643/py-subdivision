b9492f792ee5de97d0c2c4ab7af047104bc3a48a
Rajat Shrivastava <rajats643@gmail.com>
2024-11-07 14:46:42 -0800

    graph init with weights

--- scripts/graph_based_image_segmentation.py
+++ scripts/graph_based_image_segmentation.py
@@ -4,36 +4,69 @@ from typing import Dict, Set, Tuple, List
 
 import numpy as np
 
-from utils import read_image, show_image, scale_image
+from utils import setup_logging, read_image, show_image, scale_image, gaussian_blur
 import logging.config
 from random import choice
 from collections import defaultdict
+from time import perf_counter
 
 
-def get_adjacent(x: int, y: int, image: np.ndarray) -> List[Tuple[int, int]]:
+def get_adjacent_coords(x: int, y: int, image: np.ndarray) -> List[Tuple[int, int]]:
     (h, w) = image.shape[:2]
     result: List[Tuple[int, int]] = []
 
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if not (0 <= (x + i) < h) or not (
-                0 <= (y + j) < w
-            ):  # if either coord out of bounds then exit
+            x_out_of_bounds: bool = not (0 <= (x + i) < h)
+            y_out_of_bounds: bool = not (0 <= (y + j) < w)
+            same_element: bool = not (i or j)
+            if x_out_of_bounds or y_out_of_bounds or same_element:
                 continue
             result.append((x + i, y + j))
+    # logger.debug(f"({x},{y}) -> {result}")
     return result
 
 
+# TODO: finish graph implementation
+# TODO: build graph with correct weights
+# TODO: calculate mst
+# TODO: calculate cuts and regions
+# TODO: merge regions
+# TODO: segment images
+# TODO: write tests
+# TODO: remove dependency on openCV
+
+
+def calculate_weights(x: int, y: int, image: np.ndarray) -> List[Tuple[int, int]]:
+    """calculate weights of a point with respect to all adjacent points"""
+    adjacent_coords: List[Tuple[int, int]] = get_adjacent_coords(x, y, image)
+    weights: List[Tuple[int, int]] = []
+    for px, py in adjacent_coords:
+        # logger.debug(f"({px,py},{x,y}) -> {image[px][py]},{image[x][y]}")
+        weight: int = abs(int(image[px][py]) - int(image[x][y]))
+        endpoint: int = (image.shape[0] * px) + py
+        weights.append((endpoint, weight))
+    # logger.debug(f"({x},{y}) -> {weights}")
+    return weights
+
+
 class ImageGraph:
-    def __init__(self):
-        self.G: Dict[int, Set[Tuple[int, float]]] = defaultdict(set)
-        # G = {vertex: {(endpoint, weight), (endpoint, weight}, ...}
+    """build a graph from a single channel image"""
+
+    def __init__(self, image: np.ndarray):
+        # G = {vertex: {(endpoint, weight), (endpoint, weight),...}, ...}
+        if not isinstance(image, np.ndarray):
+            raise TypeError("image must be np.ndarray")
+        if len(image.shape) != 2:
+            raise TypeError("image must be single channel 2d array")
+        self.G: Dict[int, Set[Tuple[int, int]]] = defaultdict(set)
+        self._build_graph_from_image(image)
 
     def _add_vertex(self, vertex: int) -> None:
         if vertex not in self.G:
             self.G[vertex] = set()
 
-    def _add_edge(self, vertex: int, endpoint: int, weight: float) -> None:
+    def _add_edge(self, vertex: int, endpoint: int, weight: int) -> None:
         if vertex not in self.G:
             self._add_vertex(vertex)
         if endpoint not in self.G[vertex]:
@@ -42,10 +75,15 @@ class ImageGraph:
         if not (vertex, weight) in self.G[endpoint]:
             self.G[vertex].add((endpoint, weight))
 
-    def _build_graph_from_image(self, image: np.ndarray) -> np.ndarray:
-        """image likely has three channels, we do only for one"""
-
-        pass
+    def _build_graph_from_image(self, image: np.ndarray) -> None:
+        """expecting single channel image"""
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                vertex: int = (image.shape[0] * x) + y
+                connections: List[Tuple[int, int]] = calculate_weights(x, y, image)
+                self._add_vertex(vertex)
+                for endpoint, weight in connections:
+                    self._add_edge(vertex, endpoint, weight)
 
     def __str__(self) -> str:
         result: str = ""
@@ -54,30 +92,40 @@ class ImageGraph:
         return result
 
 
-def setup_logging():
-    log_config: dict = {
-        "version": 1,
-        "formatters": {
-            "short": {
-                "format": "%(funcName)s | %(levelname)s: %(message)s",
-                "datefmt": "%Y-%m-%dT%H:%M:%S%z",
-            },
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "level": logging.DEBUG,
-                "formatter": "short",
-            },
-        },
-        "loggers": {
-            "": {
-                "handlers": ["console"],
-                "level": "DEBUG",
-            },
-        },
-    }
-    logging.config.dictConfig(log_config)
+def get_test_images(
+    scale_factor: float = 0.1, channel: int = 0
+) -> Tuple[np.ndarray, np.ndarray]:
+    # setup params
+    base_path: Path = Path(os.path.dirname(os.path.dirname(__file__)))
+    test_images: list = ["building", "camera", "clothes", "island", "ski"]
+    test_image_name: str = choice(test_images)
+    test_image_path: Path = base_path / "test_images" / f"{test_image_name}.jpg"
+
+    # operations
+    image = read_image(test_image_path)
+    image = scale_image(image, scale=scale_factor)
+    image = gaussian_blur(image)
+
+    # graph testing
+    single_channel: np.ndarray = np.array(image[:, :, channel])
+    logger.info(f"fetching image: {test_image_name}  img: {image.shape}")
+    return image, single_channel
+
+
+def runtime_test(n: int = 5, sf: float = 0.1) -> None:
+    total: float = 0.0
+    logger.info(f"running init runtime test ({n})")
+    for _ in range(n):
+        _, single = get_test_images(scale_factor=sf)
+        start = perf_counter()
+        _ = ImageGraph(single)
+        result = perf_counter() - start
+        logger.debug(f" init time: {result:.3f} seconds")
+        total += result
+        del single, _
+
+    logger.info(f"total init time: {total:.3f} seconds")
+    logger.info(f"average init time: {(total/n):.3f} seconds")
 
 
 if __name__ == "__main__":
@@ -85,20 +133,13 @@ if __name__ == "__main__":
     graph based segmentation - testing only
     notes:
     """
-
-    # setup params
-    base_path: Path = Path(os.path.dirname(os.path.dirname(__file__)))
-    test_images: list = ["building", "camera", "clothes", "island", "ski"]
-    test_image: str = choice(test_images)
-    test_image_path: Path = base_path / "test_images" / f"{test_image}.jpg"
-    scale_factor: float = 0.15
+    verbose = True
 
     # setup logging
-    setup_logging()
+    setup_logging(debug_filename=Path("./graph.log"), verbose=verbose)
     logger: logging.Logger = logging.getLogger(__name__)
-    logger.debug("setup logger")
+    logger.info("setup logger")
 
     # operations
-    img = read_image(test_image_path)
-    img = scale_image(img, scale=scale_factor)
-    show_image(img, test_image)
+    # runtime_test(n=4)
+    logger.info("end of test")
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,40 @@ import cv2 as cv
 import logging.config
 
 
+def setup_logging(debug_filename: Path, verbose: bool = False):
+    handlers: list = ["console", "file"] if verbose else []
+    log_config: dict = {
+        "version": 1,
+        "formatters": {
+            "short": {
+                "format": "%(funcName)s | %(levelname)s: %(message)s",
+                "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": logging.INFO,
+                "formatter": "short",
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "level": logging.DEBUG,
+                "formatter": "short",
+                "filename": str(debug_filename),
+                "mode": "w",
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": handlers,
+                "level": "DEBUG",
+            },
+        },
+    }
+    logging.config.dictConfig(log_config)
+
+
 def scale_image(
     image: np.ndarray, scale: float, interpolation: int = cv.INTER_LINEAR
 ) -> np.ndarray:
@@ -18,6 +52,16 @@ def scale_image(
     except (cv.error, ValueError, Exception) as e:
         raise RuntimeError("failed to scale image") from e
     return resized_image if resized_image.size > 0 else image
+
+
+def gaussian_blur(image: np.ndarray, sigma: float = 0.8, k: int = 3) -> np.ndarray:
+    logger: logging.Logger = logging.getLogger(__name__)
+    try:
+        result: np.ndarray = cv.GaussianBlur(src=image, ksize=[k, k], sigmaX=sigma)
+        logger.debug(f"applied gaussian blur with sigma: {sigma}")
+    except (cv.error, Exception) as e:
+        raise RuntimeError("failed to apply gaussian blur") from e
+    return result
 
 
 def show_image(image: np.ndarray, name: str = "image") -> None:
