85e74ced800a7ef4e0bd0c74e3d9c7d6980c0859
Rajat <rajats643@gmail.com>
2025-01-22 04:08:04 -0800

    new class structure

--- scripts/graph_based_image_segmentation.py
+++ scripts/graph_based_image_segmentation.py
@@ -1,111 +1,91 @@
-import os
-from pathlib import Path
-from typing import Dict, Set, Tuple, List
+# --------------------------- #
+#   IMPORTS     #
+# --------------------------- #
 
+# built-in packages
+import os
+import logging.config
+from pathlib import Path
+from typing import Dict, Set, Tuple, List, Callable
+from random import choice, randint
+from time import perf_counter
+import heapq
+
+# internal modules
+import utils
+
+# external modules
 import numpy as np
 
-from utils import setup_logging, read_image, show_image, scale_image, gaussian_blur
-import logging.config
-from random import choice
-from collections import defaultdict
-from time import perf_counter
+# --------------------------- #
+#   CLASSES     #
+# --------------------------- #
 
 
-def get_adjacent_coords(x: int, y: int, image: np.ndarray) -> List[Tuple[int, int]]:
-    (h, w) = image.shape[:2]
-    result: List[Tuple[int, int]] = []
+class Vertex:
+    """a vertex class that contains the coordinates of a single vertex from the image"""
 
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            x_out_of_bounds: bool = not (0 <= (x + i) < h)
-            y_out_of_bounds: bool = not (0 <= (y + j) < w)
-            same_element: bool = not (i or j)
-            if x_out_of_bounds or y_out_of_bounds or same_element:
-                continue
-            result.append((x + i, y + j))
-    return result
+    def __init__(self, x: int, y: int):
+        self.x: int = x
+        self.y: int = y
+
+    def __str__(self):
+        return f"x: {self.x}, y: {self.y}"
 
 
-# TODO: calculate mst prims and kruskal
-# TODO: calculate cuts and regions
-# TODO: merge regions
-# TODO: segment images
-# TODO: write tests
-# TODO: remove dependency on openCV
+class Edge:
+    """an edge class that contains two vertices that make an edge and its weight"""
+
+    def __init__(self, v1: Vertex, v2: Vertex):
+        self.v1: Vertex = v1
+        self.v2: Vertex = v2
+        self.weight: float = 0
+
+    def set_weight(
+        self,
+        image: np.ndarray,
+        weight_function: Callable[[Vertex, Vertex, np.ndarray], float],
+    ):
+        self.weight = weight_function(self.v1, self.v2, image)
+
+    def __lt__(self, other) -> bool:
+        return self.weight < other.weight
+
+    def __str__(self):
+        return f"v1: {self.v1} v2: {self.v2} weight: {self.weight}"
 
 
-def calculate_weights(x: int, y: int, image: np.ndarray) -> List[Tuple[int, int]]:
-    """calculate weights of a point with respect to all adjacent points"""
-    adjacent_coords: List[Tuple[int, int]] = get_adjacent_coords(x, y, image)
-    weights: List[Tuple[int, int]] = []
-    for px, py in adjacent_coords:
-        weight: int = abs(int(image[px][py]) - int(image[x][y]))
-        endpoint: int = (image.shape[0] * px) + py
-        weights.append((endpoint, weight))
-    return weights
+class Component:
+    """a component that defines an area of an image using its constituent edges"""
+
+    def __init__(self, edges: Set[Edge]):
+        self.edges: Set[Edge] = edges
+
+    def edge_presence(self, edge: Edge) -> bool:
+        return edge in self.edges
 
 
-class ImageComponent:
-
-    def __init__(self, image: np.ndarray):
-        if not isinstance(image, np.ndarray):
-            raise TypeError("image must be np.ndarray")
-        if len(image.shape) != 2:
-            raise TypeError("image must be single channel 2d array")
-        self.edges: Set[Tuple[int, int, int]] = set()
-        self.vertices: Set[int] = set()
-
-    def _add_vertex(self, vertex: int) -> None:
-        self.vertices.add(vertex)
-
-    def _add_edge(self, vertex: int, endpoint: int, weight: int) -> None:
-        if not ((endpoint, vertex, weight) in self.edges):
-            self.edges.add((vertex, endpoint, weight))
-
-    def __str__(self) -> str:
-        return f"vertices: {len(self.vertices)}, edges: {len(self.edges)}"
+# --------------------------- #
+#   FUNCTIONS   #
+# --------------------------- #
 
 
-class ImageGraph:
-    """build a graph from a single channel image"""
+def merge_components(a: Component, b: Component) -> Component:
+    return Component(a.edges.union(b.edges))
 
-    def __init__(self, image: np.ndarray):
-        # G = {vertex: {(endpoint, weight), (endpoint, weight),...}, ...}
-        if not isinstance(image, np.ndarray):
-            raise TypeError("image must be np.ndarray")
-        if len(image.shape) != 2:
-            raise TypeError("image must be single channel 2d array")
-        self.G: ImageComponent = ImageComponent()
-        self._build_graph_from_image(image)
 
-    def _add_vertex(self, vertex: int) -> None:
-        if vertex not in self.G.component:
-            self.G.component[vertex] = set()
+# --------------------------- #
+#   WEIGHT FUNCTIONS    #
+# --------------------------- #
 
-    def _add_edge(self, vertex: int, endpoint: int, weight: int) -> None:
-        if vertex not in self.G.component:
-            self._add_vertex(vertex)
-        if endpoint not in self.G.component[vertex]:
-            self._add_vertex(endpoint)
 
-        if not (vertex, weight) in self.G.component[endpoint]:
-            self.G.component[vertex].add((endpoint, weight))
+def absolute_intensity(v1: Vertex, v2: Vertex, image) -> float:
+    return abs(image[v1.x][v1.y] - image[v2.x][v2.y])
 
-    def _build_graph_from_image(self, image: np.ndarray) -> None:
-        """expecting single channel image"""
-        for x in range(image.shape[0]):
-            for y in range(image.shape[1]):
-                vertex: int = (image.shape[0] * x) + y
-                connections: List[Tuple[int, int]] = calculate_weights(x, y, image)
-                self._add_vertex(vertex)
-                for endpoint, weight in connections:
-                    self._add_edge(vertex, endpoint, weight)
 
-    def __str__(self) -> str:
-        result: str = ""
-        for vertex, edges in self.G.component.items():
-            result += f"{vertex}: {edges}\n"
-        return result
+# --------------------------- #
+#   TEMP FUNCTIONS  #
+# --------------------------- #
 
 
 def get_test_images(
@@ -118,9 +98,9 @@ def get_test_images(
     test_image_path: Path = base_path / "test_images" / f"{test_image_name}.jpg"
 
     # operations
-    image = read_image(test_image_path)
-    image = scale_image(image, scale=scale_factor)
-    image = gaussian_blur(image)
+    image = utils.read_image(test_image_path)
+    image = utils.scale_image(image, scale=scale_factor)
+    image = utils.gaussian_blur(image)
 
     # graph testing
     single_channel: np.ndarray = np.array(image[:, :, channel])
@@ -128,26 +108,24 @@ def get_test_images(
     return image, single_channel
 
 
-def runtime_test(n: int = 5, sf: float = 0.1) -> None:
-    """
-
-    :param n: number of images
-    :param sf: scaling factor for the images
-    :return: None
-    """
-    total: float = 0.0
-    logger.info(f"running init runtime test ({n})")
+def get_list(n: int, image: np.ndarray) -> List[Edge]:
+    result = []
     for _ in range(n):
-        _, single = get_test_images(scale_factor=sf)
-        start = perf_counter()
-        _ = ImageGraph(single)
-        result = perf_counter() - start
-        logger.debug(f" init time: {result:.3f} seconds")
-        total += result
+        v1 = Vertex(randint(0, n), randint(0, n))
+        v2 = Vertex(randint(0, n), randint(0, n))
+        edge = Edge(
+            v1,
+            v2,
+        )
+        edge.set_weight(image, absolute_intensity)
+        result.append(edge)
 
-    logger.info(f"total init time: {total:.3f} seconds")
-    logger.info(f"average init time: {(total/n):.3f} seconds")
+    return result
 
+
+# --------------------------- #
+#   MAIN    #
+# --------------------------- #
 
 if __name__ == "__main__":
     """
@@ -157,10 +135,23 @@ if __name__ == "__main__":
     verbose = True
 
     # setup logging
-    setup_logging(debug_filename=Path("./graph.log"), verbose=verbose)
+    utils.setup_logging(debug_filename=Path("./graph.log"), verbose=verbose)
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info("setup logger")
 
     # operations
-    runtime_test(n=4, sf=0.1)
+    # runtime_test(n=4, sf=0.1)
+    sample = np.array([[x for x in range(11)] for _ in range(11)])
+
+    list_edges = get_list(10, sample)
+    for edge in list_edges:
+        print(edge)
+
+    print()
+
+    for edge in sorted(list_edges):
+        print(edge)
+
+    print(list_edges[3] > list_edges[1])
+
     logger.info("end of test")
