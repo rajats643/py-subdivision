cc7d1327e698c60aa92af4cb8a6d485cee1f50a8
Rajat <rajats643@gmail.com>
2025-02-25 02:30:07 -0800

    multiprocessing for rgb and updated mint calculation

--- scripts/graph_based_image_segmentation.py
+++ scripts/graph_based_image_segmentation.py
@@ -32,6 +32,7 @@ class FHSolver:
         self.parent: List[int] = [i for i in range(w * h)]
         self.internal_difference: List[float] = [0 for _ in range(w * h)]
         self.rank: List[int] = [1 for _ in range(w * h)]
+        self.size: List[int] = [1 for _ in range(w * h)]
 
     def find(self, x: int) -> int:
         """find the representative of this point"""
@@ -51,19 +52,22 @@ class FHSolver:
         mint_x: float = self.internal_difference[parent_x]
         mint_y: float = self.internal_difference[parent_y]
         mint: float = min(
-            mint_x + (self.k / self.rank[parent_x]),
-            mint_y + (self.k / self.rank[parent_y]),
+            mint_x + (self.k / self.size[parent_x]),
+            mint_y + (self.k / self.size[parent_y]),
         )
         if mint < weight:
             return None  # the edge doesn't meet the conditions of the FH predicate
 
         if self.rank[parent_x] < self.rank[parent_y]:
             self.parent[parent_x] = parent_y
+            self.size[parent_y] += self.size[parent_x]
         elif self.rank[parent_x] > self.rank[parent_y]:
             self.parent[parent_y] = parent_x
+            self.size[parent_x] += self.size[parent_y]
         else:
             self.parent[parent_y] = parent_x
             self.rank[parent_x] += 1
+            self.size[parent_x] += self.size[parent_y]
 
         max_edge_weight: float = max(
             self.internal_difference[x], self.internal_difference[y], weight
@@ -84,12 +88,6 @@ class FHSolver:
 # --------------------------- #
 
 
-def timeit(start: float, s: str = "") -> float:
-    result = perf_counter() - start
-    logger.info(f"{s} execution time: {result:.3f}")
-    return result
-
-
 # --------------------------- #
 #   WEIGHT FUNCTIONS    #
 # --------------------------- #
@@ -106,7 +104,7 @@ def get_test_image(
     # setup params
     base_path: Path = Path(os.path.dirname(os.path.dirname(__file__)))
     test_images: list = ["building", "camera", "clothes", "island", "ski"]
-    # test_images: list = ["clothes"]
+    # test_images: list = ["building"]
     # test_image_name: str = choice(test_images)
     for test_image_name in test_images:
         test_image_path: Path = base_path / "test_images" / f"{test_image_name}.jpg"
@@ -124,86 +122,129 @@ def get_test_image(
 
 def get_edges(image: np.ndarray) -> List[Tuple[int, int, float]]:
     h, w = image.shape
-    result = []
+    result: List[Tuple[int, int, float]] = []
 
     def form_edge(a: Tuple[int, int], b: Tuple[int, int]) -> Tuple[int, int, float]:
         return (
             a[0] * w + a[1],
             b[0] * w + b[1],
-            abs(float(image[a[0]][a[1]]) - float(image[b[0]][b[1]])),
+            abs(
+                float(image[a[0]][a[1]]) - float(image[b[0]][b[1]])
+            ),  # absolute difference in pixel value
         )
 
     for i in range(h - 1):
         for j in range(w - 1):
-            current_point: Tuple[int, int] = (i, j)
-            right_point: Tuple[int, int] = (i, j + 1)
-            bottom_point: Tuple[int, int] = (i + 1, j)
-            result.append(form_edge(current_point, right_point))
-            result.append(form_edge(current_point, bottom_point))
+            result.append(form_edge((i, j), (i, j + 1)))  # right edge
+            result.append(form_edge((i, j), (i + 1, j)))  # bottom edge
 
     for j in range(w - 1):
-        current_point: Tuple[int, int] = (h - 1, j)
-        right_point: Tuple[int, int] = (h - 1, j + 1)
-        result.append(form_edge(current_point, right_point))
+        result.append(
+            form_edge((h - 1, j), (h - 1, j + 1))
+        )  # right edges on bottom row
 
     for i in range(h - 1):
-        current_point: Tuple[int, int] = (i, w - 1)
-        bottom_point: Tuple[int, int] = (i + 1, w - 1)
-        result.append(form_edge(current_point, bottom_point))
+        result.append(
+            form_edge((i, w - 1), (i + 1, w - 1))
+        )  # bottom edges on right column
 
     return result
 
 
-def runtime_test(image, single, k=10):
-    h, w = single.shape
-    # utils.show_image(image)
-    # utils.show_image(single)
+def apply_mask(image: np.ndarray, mask: np.ndarray) -> None:
+    h, w = mask.shape
+    for i in range(h):
+        for j in range(w):
+            image[i][j] = utils.GRAYSCALE_TO_RGB_MAP[mask[i][j]]
 
-    main_start = perf_counter()
 
-    start = perf_counter()
-    edges = get_edges(single)
-    timeit(start, "get_edges")
-    logger.info(f"edges: {len(edges)}")
-    logger.info(f"expected edges: {(2 * h * w) - (h + w)}")
+def apply_triple_mask(image: np.ndarray, masks: List[np.ndarray]) -> None:
+    h, w = masks[0].shape
+    for i in range(h):
+        for j in range(w):
+            image[i][j][0] = masks[0][i][j]
+            image[i][j][1] = masks[1][i][j]
+            image[i][j][2] = masks[2][i][j]
 
-    start = perf_counter()
+
+def apply_segment_colors(image: np.ndarray) -> None:
+    h, w, c = image.shape
+    for i in range(h):
+        for j in range(w):
+            r, g, b = image[i][j]
+            value = int((0.299 * r) + (0.587 * g) + (0.114 * b))
+            image[i][j] = utils.GRAYSCALE_TO_RGB_MAP[value]
+
+
+def get_fh_segmentation_mask(
+    single_channel_image: np.ndarray,
+    k: int = 10,
+    result=None,
+    channel: int = 0,
+) -> None:
+
+    # image dimensions
+    h, w = single_channel_image.shape
+    result_mask: np.ndarray = single_channel_image.copy()
+
+    # get edges based on pixel values, sort by weight
+    edges: List[Tuple[int, int, float]] = get_edges(single_channel_image)
     edges = sorted(edges, key=lambda x: x[2])
-    # logger.info(f"edges sorted: {(perf_counter() - start):.2f} seconds")
-    timeit(start, "sorting")
 
-    s = FHSolver(h, w, k)
+    # initialize solver
+    solver: FHSolver = FHSolver(h, w, k)
 
-    start = perf_counter()
+    # form MST components
     for edge in edges:
-        s.union(*edge)
-    timeit(start, "union / mst")
+        solver.union(*edge)
 
-    start = perf_counter()
+    # resolve parents
     for i in range(h * w):
-        s.find(s.parent[i])
-    timeit(start, "resolve parents")
+        solver.find(solver.parent[i])
 
-    start = perf_counter()
+    # create segment mask
     for i in range(h):
         for j in range(w):
-            value = s.parent[i * w + j]
-            new_value = single[value // w][value % w]
-            single[i][j] = new_value
-    timeit(start, "create mask")
+            value = solver.parent[i * w + j]
+            result_mask[i][j] = single_channel_image[value // w][value % w]
 
-    start = perf_counter()
-    for i in range(h):
-        for j in range(w):
-            value = utils.GRAYSCALE_TO_RGB_MAP[single[i][j]]
-            image[i][j][0] = value[0]
-            image[i][j][1] = value[1]
-            image[i][j][2] = value[2]
-    timeit(start, "apply mask")
-    timeit(main_start, "overall")
+    # set value in shared dictionary
+    result.put((channel, result_mask))
 
-    # utils.show_image(image)
-    logger.info(f"number of components: {len(set(s.parent))}")
+
+def run_fh_rgb(image, k_scale=10):
+    # start_time = perf_counter()
+    workers: List[mp.Process] = []
+    result = mp.Queue()
+    for channel in range(3):
+        worker: mp.Process = mp.Process(
+            target=get_fh_segmentation_mask,
+            args=(
+                image[:, :, channel],
+                k_scale,
+                result,
+                channel,
+            ),
+        )
+        workers.append(worker)
+        worker.start()
+
+    masks = [0, 0, 0]
+    count = 0
+    while count < 3:
+        if not result.empty():
+            temp = result.get()
+            masks[temp[0]] = temp[1]
+            count += 1
+
+    for worker in workers:
+        worker.join()
+
+    result.close()
+    apply_triple_mask(image, masks=masks)
+    apply_segment_colors(image)
+    # end_time = perf_counter()
+    # logger.info(f" fh_time: {(end_time-start_time):.3f} seconds")
 
 
 # --------------------------- #
@@ -224,14 +265,15 @@ if __name__ == "__main__":
 
     # operations
 
-    for image, single in get_test_image(scale_factor=0.15, channel=0):
-        utils.show_image(image)
+    scale_line: list = [10_000]
+    for test_image, sc in get_test_image(scale_factor=0.15, channel=0):
+        utils.show_image(test_image)
         start_time = perf_counter()
-        runtime_test(image, single, 30)
-        runtime_test(image, np.array(image[:, :, 0]), 20)
-        runtime_test(image, np.array(image[:, :, 0]), 10)
+        for scale in scale_line:
+            run_fh_rgb(test_image[:, :, :], k_scale=scale)
         end_time = perf_counter()
-        utils.show_image(image)
+        logger.info(f" fh_time: {(end_time-start_time):.3f} seconds")
+        utils.show_image(test_image)
 
     # logger.info(f"runtime: {(end_time-start_time):.2f} seconds")
     logger.info("end of test")
